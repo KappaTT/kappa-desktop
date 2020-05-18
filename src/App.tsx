bac63c3196e47db81f39e09201fc910b1b82769b
Jeff Taylor-Chang <jeff@jefftc.com>
2020-05-18 15:25:21 -0400

    Connected app navigation and built entry

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,79 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Image, View } from 'react-native';
+import { AppLoading } from 'expo';
+import { Asset } from 'expo-asset';
+import * as Font from 'expo-font';
+import { useSelector, useDispatch } from 'react-redux';
+import { enableScreens } from 'react-native-screens';
+
+import { TRedux } from '@reducers';
+import { _auth } from '@reducers/actions';
+import { incompleteUser } from '@backend/auth';
+import { Images, theme } from '@constants';
+import AppNavigator from '@navigation/StackAppNavigator';
+import { setTopLevelNavigator } from '@navigation/NavigationService';
+
+enableScreens();
+
+const assetImages = [Images.Kappa];
+
+const cacheImages = (images: any) => {
+  return images.map((image: any) => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+};
+
+const _loadResourcesAsync = async () => {
+  await Promise.all([
+    ...cacheImages(assetImages),
+    Font.loadAsync({
+      OpenSans: require('../assets/font/OpenSans-Regular.ttf'),
+      'OpenSans-Bold': require('../assets/font/OpenSans-Bold.ttf'),
+      'OpenSans-SemiBold': require('../assets/font/OpenSans-SemiBold.ttf'),
+      'OpenSans-Light': require('../assets/font/OpenSans-Light.ttf'),
+      'PlayfairDisplay-Bold': require('../assets/font/PlayfairDisplay-Bold.ttf')
+    })
+  ]);
+};
 
 const App = () => {
-  return <View style={styles.container}></View>;
+  const loadedUser = useSelector((state: TRedux) => state.auth.loadedUser);
+  const authorized = useSelector((state: TRedux) => state.auth.authorized);
+  const user = useSelector((state: TRedux) => state.auth.user);
+
+  const [isLoadingComplete, setIsLoadingComplete] = React.useState<boolean>(false);
+  const [isNavigatorReady, setIsNavigatorReady] = React.useState<boolean>(false);
+
+  const _handleLoadingError = React.useCallback((error: any) => {
+    // In this case, you might want to report the error to your error
+    // reporting service, for example Sentry
+    console.warn(error);
+  }, []);
+
+  const _handleFinishLoading = React.useCallback(() => {
+    setIsLoadingComplete(true);
+  }, []);
+
+  if (!isLoadingComplete) {
+    return (
+      <AppLoading startAsync={_loadResourcesAsync} onError={_handleLoadingError} onFinish={_handleFinishLoading} />
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        <AppNavigator
+          ref={(navigatorRef) => {
+            setTopLevelNavigator(navigatorRef);
+            setIsNavigatorReady(true);
+          }}
+        />
+      </View>
+    );
+  }
 };
 
 const styles = StyleSheet.create({
--- src/reducers/actions/index.ts
+++ src/reducers/actions/index.ts
@@ -1,6 +1,5 @@
 import * as _auth from './auth';
 import * as _kappa from './kappa';
-import * as _prefs from './pref';
 import * as _ui from './ui';
 
-export { _auth, _kappa, _prefs, _ui };
+export { _auth, _kappa, _ui };
