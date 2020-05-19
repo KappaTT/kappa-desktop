df8cb01c504596ad1c46cb44d163c25630875b15
Jeff Taylor-Chang <jeff@jefftc.com>
2020-05-19 14:03:32 -0400

    Added support for json storage

--- src/services/asyncStorage.ts
+++ src/services/asyncStorage.ts
@@ -2,8 +2,10 @@ import { AsyncStorage } from 'react-native';
 
 import {
   setItemTemplate,
+  setJsonTemplate,
   setBatchTemplate,
   getItemTemplate,
+  getJsonTemplate,
   getBatchTemplate,
   deleteItemTemplate,
   deleteBatchTemplate
@@ -11,9 +13,13 @@ import {
 
 export const setItem = (key: string, value: any) => setItemTemplate(AsyncStorage.setItem, key, value);
 
+export const setJson = (key: string, data: any) => setJsonTemplate(AsyncStorage.setItem, key, data);
+
 export const setBatch = (parent: string, data: any) => setBatchTemplate(setItem, parent, data);
 
 export const getItem = (key: string) => getItemTemplate(AsyncStorage.getItem, key);
+
+export const getJson = (key: string) => getJsonTemplate(AsyncStorage.getItem, key);
 
 export const getBatch = (parent: string, defaultData: any, force: boolean = false) =>
   getBatchTemplate(getItem, parent, defaultData, force);
