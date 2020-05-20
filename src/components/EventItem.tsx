912e4e32bde8f6aa57a9e4afd9059db9d3aef41c
Jeff Taylor-Chang <jeff@jefftc.com>
2020-05-20 17:21:04 -0400

    Started adding details to event item

--- src/components/EventItem.tsx
+++ src/components/EventItem.tsx
@@ -24,38 +24,111 @@ import Switch from '@components/Switch';
 
 const EventItem: React.FC<{ event: TEvent }> = ({ event }) => {
   const user = useSelector((state: TRedux) => state.auth.user);
+  const loadHistory = useSelector((state: TRedux) => state.kappa.loadHistory);
   const records = useSelector((state: TRedux) => state.kappa.records);
+  const directory = useSelector((state: TRedux) => state.kappa.directory);
+  const directorySize = useSelector((state: TRedux) => state.kappa.directorySize);
+  const missedMandatory = useSelector((state: TRedux) => state.kappa.missedMandatory);
+  const isGettingAttendance = useSelector((state: TRedux) => state.kappa.isGettingAttendance);
+  const getAttendanceError = useSelector((state: TRedux) => state.kappa.getAttendanceError);
+
+  const [expanded, setExpanded] = React.useState<boolean>(false);
+  const [readyToDelete, setReadyToDelete] = React.useState<boolean>(false);
 
   const dispatch = useDispatch();
-  const dispatchSelectEvent = React.useCallback(() => dispatch(_kappa.selectEvent(event._id)), [dispatch, event]);
+  const dispatchSelectEvent = React.useCallback(() => dispatch(_kappa.selectEvent(event._id)), [dispatch, event._id]);
+  const dispatchUnselectEvent = React.useCallback(() => dispatch(_kappa.unselectEvent()), [dispatch]);
+  const dispatchGetAttendance = React.useCallback(
+    (overwrite: boolean = false) => dispatch(_kappa.getEventAttendance(user, event._id, overwrite)),
+    [dispatch, user, event._id]
+  );
+  const dispatchGetMyAttendance = React.useCallback(
+    (overwrite: boolean = false) => dispatch(_kappa.getMyAttendance(user, overwrite)),
+    [dispatch, user]
+  );
+
+  const loadData = React.useCallback(
+    (force: boolean) => {
+      if (user.privileged) {
+        if (!isGettingAttendance && (force || (!getAttendanceError && shouldLoad(loadHistory, `event-${event._id}`)))) {
+          dispatchGetAttendance(force);
+        }
+
+        setReadyToDelete(false);
+      } else {
+        if (!isGettingAttendance && (force || (!getAttendanceError && shouldLoad(loadHistory, `user-${user.email}`)))) {
+          dispatchGetMyAttendance(force);
+        }
+      }
+    },
+    [
+      user.privileged,
+      user.email,
+      isGettingAttendance,
+      getAttendanceError,
+      loadHistory,
+      event._id,
+      dispatchGetAttendance,
+      dispatchGetMyAttendance
+    ]
+  );
+
+  const onPressExpand = React.useCallback(() => {
+    setExpanded(!expanded);
+  }, [expanded]);
+
+  const attended = React.useMemo(() => {
+    return getAttendance(records, user.email, event._id);
+  }, [event._id, records, user.email]);
+
+  const excused = React.useMemo(() => {
+    return getExcuse(records, user.email, event._id);
+  }, [event._id, records, user.email]);
+
+  const recordCounts = getEventRecordCounts(records, event._id);
+
+  const recordStats = React.useMemo(() => {
+    const fraction = directorySize === 0 ? 0 : recordCounts.sum / directorySize;
+
+    return {
+      raw: fraction,
+      percent: `${Math.round(fraction * 100)}%`
+    };
+  }, [recordCounts, directorySize]);
+
+  const mandatory = React.useMemo(() => {
+    if (!user.privileged) return [];
+
+    if (event._id === '') return [];
+
+    return Object.values(getMissedMandatoryByEvent(missedMandatory, directory, event._id)).sort(sortUserByName);
+  }, [user, missedMandatory, directory, event._id]);
 
   return (
-    <TouchableOpacity onPress={dispatchSelectEvent}>
-      <View style={styles.eventContainer}>
-        <View style={styles.eventHeader}>
-          <Text style={styles.eventTitle}>{event.title}</Text>
-          <Text style={styles.eventDate}>{moment(event.start).format('h:mm A')}</Text>
+    <View style={styles.eventContainer}>
+      <View style={styles.eventHeader}>
+        <Text style={styles.eventTitle}>{event.title}</Text>
+        <Text style={styles.eventDate}>{moment(event.start).format('h:mm A')}</Text>
 
-          {event.mandatory && (
-            <Icon
-              style={styles.mandatoryIcon}
-              family="Feather"
-              name="alert-circle"
-              size={16}
-              color={theme.COLORS.PRIMARY}
-            />
-          )}
+        {event.mandatory && (
+          <Icon
+            style={styles.mandatoryIcon}
+            family="Feather"
+            name="alert-circle"
+            size={16}
+            color={theme.COLORS.PRIMARY}
+          />
+        )}
 
-          {hasValidCheckIn(records, user.email, event._id) && (
-            <Icon style={styles.checkIcon} family="Feather" name="check" size={16} color={theme.COLORS.PRIMARY_GREEN} />
-          )}
-        </View>
-
-        <View style={styles.eventDescriptionWrapper}>
-          <Text style={styles.eventDescription}>{event.description}</Text>
-        </View>
+        {hasValidCheckIn(records, user.email, event._id) && (
+          <Icon style={styles.checkIcon} family="Feather" name="check" size={16} color={theme.COLORS.PRIMARY_GREEN} />
+        )}
       </View>
-    </TouchableOpacity>
+
+      <View style={styles.eventDescriptionWrapper}>
+        <Text style={styles.eventDescription}>{event.description}</Text>
+      </View>
+    </View>
   );
 };
 
