142dddf552619948e78c7308c1d95ea08e3d6730
Jeff Taylor-Chang <jeff@jefftc.com>
2020-05-20 12:00:08 -0400

    Added event section list to events content

--- src/screens/content/EventsContent.tsx
+++ src/screens/content/EventsContent.tsx
@@ -1,24 +1,90 @@
 import React from 'react';
-import { StyleSheet, View, Text, ActivityIndicator, TouchableOpacity, ScrollView } from 'react-native';
+import { StyleSheet, View, Text, ActivityIndicator, TouchableOpacity, ScrollView, SectionList } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import { useIsFocused } from 'react-navigation-hooks';
+import moment from 'moment';
 
 import { ParamType } from '@navigation/NavigationTypes';
 import { TRedux } from '@reducers';
 import { _auth, _kappa } from '@reducers/actions';
 import { theme } from '@constants';
+import { TEvent } from '@backend/kappa';
 import { HEADER_HEIGHT } from '@services/utils';
+import { hasValidCheckIn, getEventById, shouldLoad } from '@services/kappaService';
 import { Header, Icon } from '@components';
 
 const EventsContent: React.FC<{
   navigation: ParamType;
 }> = ({ navigation }) => {
-  const user = useSelector((state: TRedux) => state.auth.user);
+  const isFocused = useIsFocused();
 
+  const user = useSelector((state: TRedux) => state.auth.user);
+  const loadHistory = useSelector((state: TRedux) => state.kappa.loadHistory);
+  const records = useSelector((state: TRedux) => state.kappa.records);
+  const isGettingEvents = useSelector((state: TRedux) => state.kappa.isGettingEvents);
+  const getEventsError = useSelector((state: TRedux) => state.kappa.getEventsError);
+  const isGettingDirectory = useSelector((state: TRedux) => state.kappa.isGettingDirectory);
+  const getDirectoryError = useSelector((state: TRedux) => state.kappa.getDirectoryError);
+  const isGettingAttendance = useSelector((state: TRedux) => state.kappa.isGettingAttendance);
+  const getAttendanceError = useSelector((state: TRedux) => state.kappa.getAttendanceError);
+  const isGettingExcuses = useSelector((state: TRedux) => state.kappa.isGettingExcuses);
+  const getExcusesError = useSelector((state: TRedux) => state.kappa.getExcusesError);
+  const events = useSelector((state: TRedux) => state.kappa.events);
+  const eventSections = useSelector((state: TRedux) => state.kappa.eventSections);
+  const upcomingSections = useSelector((state: TRedux) => state.kappa.upcomingSections);
+  const editingEventId = useSelector((state: TRedux) => state.kappa.editingEventId);
+  const getEventsErrorMessage = useSelector((state: TRedux) => state.kappa.getEventsErrorMessage);
+
+  const [refreshing, setRefreshing] = React.useState<boolean>(
+    isGettingEvents || isGettingDirectory || isGettingAttendance
+  );
   const [showing, setShowing] = React.useState<'Full Year' | 'Upcoming'>('Upcoming');
 
   const dispatch = useDispatch();
+  const dispatchGetEvents = React.useCallback(() => dispatch(_kappa.getEvents(user)), [dispatch, user]);
+  const dispatchGetMyAttendance = React.useCallback(
+    (overwrite: boolean = false) => dispatch(_kappa.getMyAttendance(user, overwrite)),
+    [dispatch, user]
+  );
+  const dispatchGetDirectory = React.useCallback(() => dispatch(_kappa.getDirectory(user)), [dispatch, user]);
+  const dispatchGetExcuses = React.useCallback(() => dispatch(_kappa.getExcuses(user)), [dispatch, user]);
+  const dispatchSelectEvent = React.useCallback((eventId: string) => dispatch(_kappa.selectEvent(eventId)), [dispatch]);
   const dispatchEditNewEvent = React.useCallback(() => dispatch(_kappa.editNewEvent()), [dispatch]);
+  const dispatchSaveEditEvent = React.useCallback(
+    (event: Partial<TEvent>, eventId?: string) => dispatch(_kappa.saveEditEvent(user, event, eventId)),
+    [dispatch, user]
+  );
+  const dispatchCancelEditEvent = React.useCallback(() => dispatch(_kappa.cancelEditEvent()), [dispatch]);
+
+  const scrollRef = React.useRef(undefined);
+
+  const loadData = React.useCallback(
+    (force: boolean) => {
+      if (!isGettingEvents && (force || (!getEventsError && shouldLoad(loadHistory, 'events')))) dispatchGetEvents();
+      if (!isGettingDirectory && (force || (!getDirectoryError && shouldLoad(loadHistory, 'directory'))))
+        dispatchGetDirectory();
+      if (!isGettingAttendance && (force || (!getAttendanceError && shouldLoad(loadHistory, `user-${user.email}`))))
+        dispatchGetMyAttendance(force);
+      if (!isGettingExcuses && (force || (!getExcusesError && shouldLoad(loadHistory, 'excuses'))))
+        dispatchGetExcuses();
+    },
+    [
+      isGettingEvents,
+      getEventsError,
+      loadHistory,
+      dispatchGetEvents,
+      isGettingDirectory,
+      getDirectoryError,
+      dispatchGetDirectory,
+      isGettingAttendance,
+      getAttendanceError,
+      user.email,
+      dispatchGetMyAttendance,
+      isGettingExcuses,
+      getExcusesError,
+      dispatchGetExcuses
+    ]
+  );
 
   const onPressShowing = React.useCallback(() => {
     if (showing === 'Upcoming') {
@@ -27,6 +93,65 @@ const EventsContent: React.FC<{
       setShowing('Upcoming');
     }
   }, [showing]);
+
+  React.useEffect(() => {
+    if (isFocused && user.sessionToken) {
+      loadData(false);
+    }
+  }, [isFocused, loadData, user.sessionToken]);
+
+  const keyExtractor = (item: TEvent, index) => {
+    return `${item._id}-${index}`;
+  };
+
+  const renderSectionHeader = ({ section: { title, data } }) => {
+    return (
+      <View style={styles.sectionHeaderContainer}>
+        <Text style={styles.sectionHeaderText}>{moment(title).format('ddd LL')}</Text>
+      </View>
+    );
+  };
+
+  const renderItem = ({ item }: { item: TEvent }) => {
+    return (
+      <React.Fragment>
+        <TouchableOpacity onPress={() => dispatchSelectEvent(item._id)}>
+          <View style={styles.eventContainer}>
+            <View style={styles.eventHeader}>
+              <Text style={styles.eventTitle}>{item.title}</Text>
+              <Text style={styles.eventDate}>{moment(item.start).format('h:mm A')}</Text>
+
+              {item.mandatory && (
+                <Icon
+                  style={styles.mandatoryIcon}
+                  family="Feather"
+                  name="alert-circle"
+                  size={16}
+                  color={theme.COLORS.PRIMARY}
+                />
+              )}
+
+              {hasValidCheckIn(records, user.email, item._id) && (
+                <Icon
+                  style={styles.checkIcon}
+                  family="Feather"
+                  name="check"
+                  size={16}
+                  color={theme.COLORS.PRIMARY_GREEN}
+                />
+              )}
+            </View>
+
+            <View style={styles.eventDescriptionWrapper}>
+              <Text style={styles.eventDescription}>{item.description}</Text>
+            </View>
+          </View>
+        </TouchableOpacity>
+
+        <View style={styles.separator} />
+      </React.Fragment>
+    );
+  };
 
   return (
     <View style={styles.container}>
@@ -41,7 +166,19 @@ const EventsContent: React.FC<{
       </Header>
 
       <View style={styles.content}>
-        <ScrollView></ScrollView>
+        <SectionList
+          ref={(ref) => (scrollRef.current = ref)}
+          sections={showing === 'Upcoming' ? upcomingSections : eventSections}
+          keyExtractor={keyExtractor}
+          renderSectionHeader={renderSectionHeader}
+          renderItem={renderItem}
+          ListEmptyComponent={
+            <React.Fragment>
+              <Text style={styles.errorMessage}>{getEventsErrorMessage || 'No upcoming events'}</Text>
+            </React.Fragment>
+          }
+          onScrollToIndexFailed={() => {}}
+        />
       </View>
     </View>
   );
@@ -68,6 +205,64 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     bottom: 0
+  },
+  sectionHeaderContainer: {
+    backgroundColor: theme.COLORS.WHITE
+  },
+  sectionHeaderText: {
+    marginTop: 4,
+    marginBottom: 8,
+    marginLeft: 16,
+    fontFamily: 'OpenSans-Bold',
+    fontSize: 17
+  },
+  separator: {
+    marginHorizontal: 16,
+    marginBottom: 16,
+    borderBottomColor: theme.COLORS.LIGHT_BORDER,
+    borderBottomWidth: 1
+  },
+  eventContainer: {
+    paddingHorizontal: 16,
+    paddingTop: 8,
+    paddingBottom: 16
+  },
+  eventHeader: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    marginBottom: 8,
+    flexWrap: 'wrap'
+  },
+  eventTitle: {
+    fontFamily: 'OpenSans-SemiBold',
+    fontSize: 13
+  },
+  eventDate: {
+    marginLeft: 8,
+    fontFamily: 'OpenSans',
+    fontSize: 13,
+    color: theme.COLORS.DARK_GRAY
+  },
+  mandatoryIcon: {
+    marginLeft: 8
+  },
+  checkIcon: {
+    marginLeft: 8
+  },
+  eventDescriptionWrapper: {
+    marginTop: 8,
+    marginBottom: 12
+  },
+  eventDescription: {
+    fontFamily: 'OpenSans',
+    fontSize: 15
+  },
+  errorMessage: {
+    marginTop: '50%',
+    textAlign: 'center',
+    fontFamily: 'OpenSans'
   }
 });
 
