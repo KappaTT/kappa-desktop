4bdb9c980cd40a610076f276ea22a4410f507333
Jeff Taylor-Chang <jeff@jefftc.com>
2020-05-22 15:30:02 -0400

    Fixed switch overlay bug

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch as NativeSwitch } from 'react-native';
+import { Switch as NativeSwitch, TouchableOpacity } from 'react-native';
 
 import { theme } from '@constants';
 
@@ -16,18 +16,25 @@ const Switch: React.FC<{
   trackColor = theme.COLORS.PRIMARY,
   onValueChange = (newValue: boolean) => {}
 }) => {
+  const onPress = React.useCallback(() => {
+    // Using touchable to intercept and mimic behavior due to bug that causes focus overlay on switch if pressed
+    onValueChange(!value);
+  }, [onValueChange, value]);
+
   return (
-    <NativeSwitch
-      value={value}
-      thumbColor="#FFF"
-      thumbTintColor={thumbColor}
-      // @ts-ignore
-      activeThumbColor={thumbColor}
-      onTintColor={`${trackColor}50`}
-      tintColor={backgroundColor}
-      trackColor={{ false: trackColor, true: trackColor }}
-      onValueChange={onValueChange}
-    />
+    <TouchableOpacity activeOpacity={0.8} onPress={onPress}>
+      <NativeSwitch
+        pointerEvents="none"
+        value={value}
+        thumbColor="#FFF"
+        thumbTintColor={thumbColor}
+        // @ts-ignore
+        activeThumbColor={thumbColor}
+        onTintColor={`${trackColor}50`}
+        tintColor={backgroundColor}
+        trackColor={{ false: trackColor, true: trackColor }}
+      />
+    </TouchableOpacity>
   );
 };
 
