3834f1eff32a943aacf20d6398c71dd68c769a99
Jeff Taylor-Chang <jeff@jefftc.com>
2020-05-23 11:01:48 -0400

    Added production google client id

--- src/components/GoogleSignInButton.tsx
+++ src/components/GoogleSignInButton.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import GoogleLogin, { GoogleLoginResponse } from 'react-google-login';
 
+export const GOOGLE_CLIENT_ID =
+  process.env.NODE_ENV !== 'development'
+    ? '223233671218-l6tkqeif7rb8hj35ek87sci396nmt37r.apps.googleusercontent.com'
+    : '223233671218-mujcmkmt5plupe2c5piurpvp1h435pgu.apps.googleusercontent.com';
+
 const GoogleSignInButton: React.FC<{
   onSuccess(payload: GoogleLoginResponse): void;
   onFailure(error: any): void;
 }> = ({ onSuccess, onFailure }) => {
   return (
     <GoogleLogin
-      clientId="223233671218-mujcmkmt5plupe2c5piurpvp1h435pgu.apps.googleusercontent.com"
+      clientId={GOOGLE_CLIENT_ID}
       buttonText="Sign in with Google"
       cookiePolicy="single_host_origin"
       onSuccess={onSuccess}
