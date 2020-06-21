b893922e45ba616dcf1a106bc91a45fa2474cc17
Jeff Taylor-Chang <jeff@jefftc.com>
2020-06-21 17:18:20 -0400

    Started profile page

--- src/screens/content/ProfileContent.tsx
+++ src/screens/content/ProfileContent.tsx
@@ -8,7 +8,8 @@ import { ParamType } from '@navigation/NavigationTypes';
 import { TRedux } from '@reducers';
 import { _auth, _kappa, _nav } from '@reducers/actions';
 import { theme } from '@constants';
-import { HEADER_HEIGHT } from '@services/utils';
+import { HEADER_HEIGHT, isEmpty } from '@services/utils';
+import { shouldLoad, sortEventsByDateReverse } from '@services/kappaService';
 import { Header, Icon } from '@components';
 
 const ProfileContent: React.FC<{
@@ -16,10 +17,99 @@ const ProfileContent: React.FC<{
 }> = ({ navigation }) => {
   const isFocused = useIsFocused();
 
+  const user = useSelector((state: TRedux) => state.auth.user);
+  const loadHistory = useSelector((state: TRedux) => state.kappa.loadHistory);
+  const events = useSelector((state: TRedux) => state.kappa.events);
+  const isGettingEvents = useSelector((state: TRedux) => state.kappa.isGettingEvents);
+  const getEventsError = useSelector((state: TRedux) => state.kappa.getEventsError);
+  const gmCount = useSelector((state: TRedux) => state.kappa.gmCount);
+  const records = useSelector((state: TRedux) => state.kappa.records);
+  const isGettingAttendance = useSelector((state: TRedux) => state.kappa.isGettingAttendance);
+  const getAttendanceError = useSelector((state: TRedux) => state.kappa.getAttendanceError);
+  const missedMandatory = useSelector((state: TRedux) => state.kappa.missedMandatory);
+  const points = useSelector((state: TRedux) => state.kappa.points);
+  const isGettingPoints = useSelector((state: TRedux) => state.kappa.isGettingPoints);
+  const getPointsError = useSelector((state: TRedux) => state.kappa.getPointsError);
+
+  const dispatch = useDispatch();
+  const dispatchEdit = React.useCallback(() => dispatch(_auth.showOnboarding(true)), [dispatch]);
+  const dispatchGetEvents = React.useCallback(() => dispatch(_kappa.getEvents(user)), [dispatch, user]);
+  const dispatchGetMyAttendance = React.useCallback(
+    (overwrite: boolean = false) => dispatch(_kappa.getMyAttendance(user, overwrite)),
+    [dispatch, user]
+  );
+  const dispatchGetPoints = React.useCallback(() => dispatch(_kappa.getPointsByUser(user, user.email)), [
+    dispatch,
+    user
+  ]);
+
+  const refreshing = React.useMemo(() => isGettingEvents && isGettingAttendance && isGettingPoints, [
+    isGettingAttendance,
+    isGettingEvents,
+    isGettingPoints
+  ]);
+
+  const loadData = React.useCallback(
+    (force: boolean) => {
+      if (!isGettingEvents && (force || (!getEventsError && shouldLoad(loadHistory, 'events')))) dispatchGetEvents();
+      if (!isGettingAttendance && (force || (!getAttendanceError && shouldLoad(loadHistory, `user-${user.email}`))))
+        dispatchGetMyAttendance(force);
+      if (!isGettingPoints && (force || (!getPointsError && shouldLoad(loadHistory, `points-${user.email}`))))
+        dispatchGetPoints();
+    },
+    [
+      isGettingEvents,
+      getEventsError,
+      loadHistory,
+      dispatchGetEvents,
+      isGettingAttendance,
+      getAttendanceError,
+      user.email,
+      dispatchGetMyAttendance,
+      isGettingPoints,
+      getPointsError,
+      dispatchGetPoints
+    ]
+  );
+
+  const onRefresh = React.useCallback(() => {
+    loadData(true);
+  }, [loadData]);
+
+  const mandatory = React.useMemo(() => {
+    if (!user.privileged) return [];
+
+    if (isEmpty(missedMandatory[user.email])) return [];
+
+    return Object.values(missedMandatory[user.email]).sort(sortEventsByDateReverse);
+  }, [user, missedMandatory]);
+
+  React.useEffect(() => {
+    if (isFocused && user.sessionToken) {
+      loadData(false);
+    }
+  }, [isFocused, loadData, user.sessionToken]);
+
   return (
     <View style={styles.container}>
       <Header title="Profile">
-        <View style={styles.headerChildren}></View>
+        <View style={styles.headerChildren}>
+          <View style={styles.refreshContainer}>
+            {refreshing ? (
+              <ActivityIndicator style={styles.refreshIcon} color={theme.COLORS.PRIMARY} />
+            ) : (
+              <TouchableOpacity onPress={onRefresh}>
+                <Icon
+                  style={styles.refreshIcon}
+                  family="Feather"
+                  name="refresh-cw"
+                  size={17}
+                  color={theme.COLORS.PRIMARY}
+                />
+              </TouchableOpacity>
+            )}
+          </View>
+        </View>
       </Header>
 
       <View style={styles.content}></View>
