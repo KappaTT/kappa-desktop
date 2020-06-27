27a6834452f34c7d49e5db70838467525a936f8f
Jeff Taylor-Chang <jeff@jefftc.com>
2020-06-27 17:33:43 -0400

    Started messages page

--- src/screens/content/MessagesContent.tsx
+++ src/screens/content/MessagesContent.tsx
@@ -7,14 +7,66 @@ import moment from 'moment';
 import { ParamType } from '@navigation/NavigationTypes';
 import { TRedux } from '@reducers';
 import { _auth, _kappa, _nav } from '@reducers/actions';
+import { TExcuse } from '@backend/kappa';
+import { getExcusedEvents, getEventById, sortEventsByDateReverse, shouldLoad } from '@services/kappaService';
 import { theme } from '@constants';
-import { HEADER_HEIGHT } from '@services/utils';
+import { HEADER_HEIGHT, isEmpty } from '@services/utils';
 import { Header, Icon } from '@components';
 
 const MessagesContent: React.FC<{
   navigation: ParamType;
 }> = ({ navigation }) => {
   const isFocused = useIsFocused();
+
+  const user = useSelector((state: TRedux) => state.auth.user);
+  const loadHistory = useSelector((state: TRedux) => state.kappa.loadHistory);
+  const directory = useSelector((state: TRedux) => state.kappa.directory);
+  const events = useSelector((state: TRedux) => state.kappa.events);
+  const records = useSelector((state: TRedux) => state.kappa.records);
+  const pendingExcusesArray = useSelector((state: TRedux) => state.kappa.pendingExcusesArray);
+  const isGettingExcuses = useSelector((state: TRedux) => state.kappa.isGettingExcuses);
+  const getExcusesError = useSelector((state: TRedux) => state.kappa.getExcusesError);
+
+  const dispatch = useDispatch();
+  const dispatchGetExcuses = React.useCallback(() => dispatch(_kappa.getExcuses(user)), [dispatch, user]);
+
+  const refreshing = React.useMemo(() => isGettingExcuses, [isGettingExcuses]);
+
+  const excused = getExcusedEvents(records, user.email);
+  const excusedArray = Object.values(excused)
+    .filter((excuse) => excuse.approved)
+    .map((excuse: TExcuse) => {
+      const event = getEventById(events, excuse.eventId);
+
+      if (isEmpty(event)) return null;
+
+      return {
+        ...excuse,
+        title: event.title,
+        start: event.start,
+        prettyStart: moment(event.start).format('ddd LLL')
+      };
+    })
+    .filter((excuse) => excuse !== null)
+    .sort(sortEventsByDateReverse);
+
+  const loadData = React.useCallback(
+    (force: boolean) => {
+      if (!isGettingExcuses && (force || (!getExcusesError && shouldLoad(loadHistory, 'excuses'))))
+        dispatchGetExcuses();
+    },
+    [isGettingExcuses, getExcusesError, loadHistory, dispatchGetExcuses]
+  );
+
+  const onRefresh = React.useCallback(() => {
+    loadData(true);
+  }, [loadData]);
+
+  React.useEffect(() => {
+    if (isFocused && user.sessionToken) {
+      loadData(false);
+    }
+  }, [user.sessionToken, isFocused, loadData]);
 
   return (
     <View style={styles.container}>
